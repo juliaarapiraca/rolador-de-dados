0f81af6cfb7074ed5964c507f4ebc558065c8c2f
Pedro Henrique <phd19994@gmail.com>
2022-06-11 12:42:40 -0300

    folders standarized

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+// search for better forms to insert styles, via css, themes or other ways
+// inser main styles for fontFamily, fontSizes, and let it standarized
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
